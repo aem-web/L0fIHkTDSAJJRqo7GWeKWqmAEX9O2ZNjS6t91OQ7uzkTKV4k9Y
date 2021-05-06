8ea3759d68b76f0006652bab4f81e4a2636c301b
aemweb <81826829+FoxToolz@users.noreply.github.com>
2021-05-06 10:55:48 -0400

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {headerFunction()};
 function headerFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        document.getElementById("header").style.background = "linear-gradient(-45deg, #c7ca00, #00e280)";
+        document.getElementById("header").style.background = "linear-gradient(-45deg, #00c06d, #00d478)";
     } else {
         document.getElementById("header").style.background = "none";
         document.getElementById("header").style.background = "rgba(0,0,0,0.1)";
